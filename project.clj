6fcd7b898f81c15eb5965f1b3992a6667cd1a62b
Alexander Kiel <alexanderkiel@gmx.net>
2018-01-09 16:10:35 +0100

    Add Linting

--- project.clj
+++ project.clj
@@ -6,7 +6,12 @@
 
   :dependencies [[org.clojure/clojure "1.9.0"]]
 
+  :plugins [[jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]]
+
   :profiles {:dev
              {:dependencies [[com.datomic/datomic-free "0.9.5385"]
                              [org.clojars.akiel/iota "0.1"]
-                             [org.clojure/test.check "0.9.0"]]}})
+                             [org.clojure/test.check "0.9.0"]]}}
+
+  :aliases
+  {"lint" ["eastwood" "{:exclude-linters [:constant-test :suspicious-test]}"]})
