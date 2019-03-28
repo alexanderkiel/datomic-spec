fb62b101597c2f422142c76754e632772af4dca0
Alexander Kiel <alexanderkiel@gmx.net>
2019-03-28 18:42:57 +0100

    Release v0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.akiel/datomic-spec "0.4-SNAPSHOT"
+(defproject org.clojars.akiel/datomic-spec "0.4"
   :description "Specs for Datomic"
   :url "https://github.com/alexanderkiel/datomic-spec"
   :license {:name "Eclipse Public License"
